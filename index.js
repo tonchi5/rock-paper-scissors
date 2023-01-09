504876d7570fc490b9fb72b6c74c1343ad314f2b
tonchi5 <tonka.brcic@hotmail.com>
2023-01-09 18:35:21 +0100

    Update index,js

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
-const choice = ["Rock", "Paper", "Scissors"];
-const computerChoice = Math.floor(Math.random() * choice.length);
-console.log(computerChoice, choice[computerChoice]);
+const choices = ["rock", "paper", "scissors"];
+
+function playRound() {
+    const playerSelection = playerChoice();
+    const computerSelection= computerChoice();
+    console.log(computerSelection);
+    const winner = checkWinner(playerSelection,computerSelection);
+    console.log(winner)
+    } 
+
+function game() {
+    playRound();
+  }
+
+function computerChoice() {
+    return choices[Math.floor(Math.random() * choices.length)];
+}
+
+function playerChoice() {
+    let input = prompt("Type rock, paper or scissors:");
+    input = input.toLowerCase();
+}
+
+function checkWinner(choiceP, choiceC) {
+    if (choiceP === choiceC) {
+        return "It's a tie!";
+    } 
+    else if (choiceP === "rock" && choiceC === "scissors" || choiceP === "paper" && choiceC === "rock"|| choiceP === "scissors" && choiceC === "paper") {
+        return "You win!";
+    }
+    else {
+        return "You lose!";
+    } 
+}
+
+game();
+   
+ 
