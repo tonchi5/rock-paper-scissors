5de994139e8d3359c12d6a32c8da91ed7493a87e
tonchi5 <tonka.brcic@hotmail.com>
2023-01-10 19:47:01 +0100

    Update index.js some things are not working properly

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const choices = ["rock", "paper", "scissors"];
+let playerScore = 0;
+let computerScore = 0;
 
 function playRound() {
     const playerSelection = playerChoice();
@@ -6,10 +8,13 @@ function playRound() {
     console.log(computerSelection);
     const winner = checkWinner(playerSelection,computerSelection);
     console.log(winner)
-    } 
+    }
 
 function game() {
-    playRound();
+    for (let i = 0; i < 5; i++) {
+    playRound();}
+    //winCondition();}
+    winCondition();
   }
 
 function computerChoice() {
@@ -18,20 +23,37 @@ function computerChoice() {
 
 function playerChoice() {
     let input = prompt("Type rock, paper or scissors:");
+    while (input == null) {
+        input = prompt("Type rock, paper or scissors!");
+    }
     input = input.toLowerCase();
+    return input;
 }
 
 function checkWinner(choiceP, choiceC) {
     if (choiceP === choiceC) {
         return "It's a tie!";
     } 
-    else if (choiceP === "rock" && choiceC === "scissors" || choiceP === "paper" && choiceC === "rock"|| choiceP === "scissors" && choiceC === "paper") {
+    else if (
+    choiceP === "rock" && choiceC === "scissors" || 
+    choiceP === "paper" && choiceC === "rock"|| 
+    choiceP === "scissors" && choiceC === "paper") {
         return "You win!";
     }
     else {
-        return "You lose!";
+        return "Try again!";
     } 
 }
+
+function winCondition() {
+    if (playerScore == 5) {
+      return "Congratulations!";
+    }
+    if (computerScore == 5) {
+      return "Game over!";
+    }
+  }
+
 
 game();
    
